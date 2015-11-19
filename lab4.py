74f9499b9093ebdac4ac6b1c23b61cc5aef11ff1
Bertha <bertha.chan@mail.utoronto.ca>
2015-11-19 14:01:21 -0500

    completed

--- lab4.py
+++ lab4.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 """ Graded Lab #4 for Inf1340, Fall 2015
-
     This program computes federal and provincial sales tax.
     Assume that the provincial sales tax is 5 percent and
     the federal sales tax is 2.5 percent. It displays the
@@ -18,11 +17,30 @@ __license__ = "MIT License"
 # Rewrite this code to use global constants, local variables and functions
 # Output the text to a file instead of printing it
 
+PROVINCIAL_TAX = 0.05
+FEDERAL_TAX = 0.025
+
+
+def calc_provincial_tax(purchase):
+    return purchase * PROVINCIAL_TAX
+
+
+def calc_federal_tax(purchase):
+    return purchase * FEDERAL_TAX
+
 
 def bill_of_sale(purchase):
+    prov_tax = calc_provincial_tax(purchase)
+    fed_tax = calc_federal_tax(purchase)
 
-    print ("Amount of purchase: {0:.2f}".format(purchase))
-    print ("Provincial tax: {0:.2f}".format(purchase * .05))
-    print ("Federal tax: {0:.2f}".format(purchase * .025))
-    print ("Total tax: {0:.2f}".format(purchase * .075))
-    print ("Total sale: {0:.2f}".format(purchase * 1.075))
+    file_name = "billofsales.txt"
+
+    with open(file_name, 'w') as output_file:
+        output_file.write("Amount of purchase: {0:.2f} \n".format(purchase))
+        output_file.write("Provincial tax: {0:.2f} \n".format(prov_tax))
+        output_file.write("Federal tax: {0:.2f} \n".format(fed_tax))
+        output_file.write("Total tax: {0:.2f} \n".format(prov_tax + fed_tax))
+        output_file.write("Total sale: {0:.2f} \n".format(purchase + prov_tax + fed_tax))
+
+
+bill_of_sale(100)
